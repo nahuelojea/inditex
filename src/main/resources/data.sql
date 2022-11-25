88bceddb322d5ae3ca32fce5ff9a534e5fb01995
XY7390OJ <naojea@paradigmadigital.com>
2022-11-25 10:39:16 -0300

    Add H2 database configuration

---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO prices (brand_id, star_date, end_date, price_list, product_id, priority, price, curr) VALUES (1, '2020-06-14-00.00.00', '2020-12-31-23.59.59', 1, 35455, 0, 35.50, 'EUR');
